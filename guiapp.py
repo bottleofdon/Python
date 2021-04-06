abc8f842e042c3b0685d04d083ecad5f39c001b8
Yotaro <enomoto.yotaro@gmail.com>
2021-04-06 14:49:31 +0900

    Adding Buttons (open file & run apps)

--- guiapp.py
+++ guiapp.py
@@ -10,4 +10,12 @@ canvas.pack()
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
+openFile = tk.Button(root, text="Open File", padx=10,
+                     pady=5, fg="black", bg="#263D42")
+openFile.pack()
+
+runApps = tk.Button(root, text="Run App", padx=10,
+                    pady=5, fg="black", bg="#263D42")
+runApps.pack()
+
 root.mainloop()
