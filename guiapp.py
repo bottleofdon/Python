c1a81537e1d8d6d84078279c57e29857edfb3b37
Yotaro <enomoto.yotaro@gmail.com>
2021-04-06 14:57:47 +0900

    added function addApps

--- guiapp.py
+++ guiapp.py
@@ -4,6 +4,12 @@ import os
 
 root = tk.Tk()
 
+
+def addApps():
+    filename = filedialog.askopenfilename(initialdir="/", title="Select File",
+                                          filetypes=(("executable", "*.APP"), ("all files", "*.*")))
+
+
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
 canvas.pack()
 
@@ -11,7 +17,7 @@ frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
 openFile = tk.Button(root, text="Open File", padx=10,
-                     pady=5, fg="black", bg="#263D42")
+                     pady=5, fg="black", bg="#263D42", command=addApps)
 openFile.pack()
 
 runApps = tk.Button(root, text="Run App", padx=10,
